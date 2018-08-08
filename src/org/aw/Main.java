3eb5df6ce25dc70d6e1fb1e0df306e30900b193c
Wacław Wojcieszko <wojciech.waclawik@gmail.com>
2018-08-08 21:46:17 +0200

    For start

---
+++ src/org/aw/Main.java
@@ -0,0 +1,8 @@
+package org.aw;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
---
+++ src/org/aw/baal/BattleRing.java
@@ -0,0 +1,7 @@
+package org.aw.baal;
+
+public class BattleRing {
+
+
+
+}
